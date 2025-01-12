c8a447585da26bb2288a52fb730cbf1fa2155f58
Vasilis <goutz@softweb.gr>
2025-01-13 01:10:10 +0200

    Changes in login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,19 +4,25 @@
   <div class="login-container-inner z-10 p-6 bg-white rounded-xl shadow-lg max-w-[500px]">
     <div class="login-prompt-container text-center">
       <p class="login-prompt text-2xl font-bold">Login with your email.</p>
-      <p class="login-info text-gray-300 text-gray-400 mt-2 font-semibold">Use your email and the password given to you in order to login and access the admin.</p>
+      <p class="login-info text-gray-300 text-gray-400 mt-3 font-semibold">Use your email and the password given to you in order to login and access the admin.</p>
     </div>
-    <form id="login-form" action="" method="POST">
+    <form id="login-form" class="mt-6" action="" method="POST">
       <div class="login-field email-field">
-        <label for="login-email-field">Email</label>
-        <input id="login-email-field" type="text"/>
+        <label for="login-email-field" class="sr-only">Email</label>
+        <div class="input-container flex items-center gap-2 p-3 border border-gray-100 rounded-xl bg-gray-100">
+          <span class="field-icon block w-[20px] text-gray-500">@svg("envelope")</span>
+          <input id="login-email-field" class="bg-gray-100 outline-none w-full" type="text" placeholder="Email"/>
+        </div>
       </div>
-      <div class="login-field password-field">
-        <label for="login-password-field">Password</label>
-        <input id="login-password-field" type="password"/>
+      <div class="login-field password-field mt-3">
+        <label for="login-password-field" class="sr-only">Password</label>
+        <div class="input-container flex items-center gap-2 p-3 border border-gray-100 rounded-xl bg-gray-100">
+          <span class="field-icon block w-[20px] text-gray-500">@svg("lock")</span>
+          <input id="login-password-field" class="bg-gray-100 outline-none w-full" type="password" placeholder="Password"/>
+        </div>
       </div>
-      <div class="login-actions">
-        <button class="login-button" type="submit">Login</button>
+      <div class="login-actions mt-6 flex justify-center">
+        <button class="login-button block w-1/2 p-3 rounded-xl text-white font-bold bg-gray-800 hover:bg-gray-600" type="submit">Login</button>
       </div>
     </form>
   </div>
